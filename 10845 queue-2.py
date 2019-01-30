faf66db8230a7e2d293a5d036af7acf8a3005bae
Denver LEE <43673933+AlmightyDenver@users.noreply.github.com>
2019-01-30 23:34:18 +0900

    difference with queue and queue-2
    In BOJ can't use collections, so in queue just print(q[0]) and del q.[0] instead of popleft()
    In queue-2 used deque([])

---
+++ 10845 queue-2.py
@@ -0,0 +1,45 @@
+import sys
+import collections
+class Q:
+    def __init__(self):
+        self.q = collections.deque([])
+    def push(self, x):
+        self.q.append(x)
+    def pop(self):
+        if self.q:
+            print(self.q.popleft())
+        else:
+            print(-1)
+    def size(self):
+        print(len(self.q))
+    def empty(self):
+        if self.q:
+            print(0)
+        else:
+            print(1)
+    def front(self):
+        if self.q:
+            print(self.q[0])
+        else:
+            print(-1)
+    def back(self):
+        if self.q:
+            print(self.q[-1])
+        else:
+            print(-1)
+
+que = Q()
+for _ in range(int(input())):
+    cmd = sys.stdin.readline().rstrip()
+    if cmd[:4] == 'push':
+        que.push(int(cmd[5:]))
+    elif cmd == 'pop':
+        que.pop()
+    elif cmd == 'size':
+        que.size()
+    elif cmd == 'empty':
+        que.empty()
+    elif cmd == 'front':
+        que.front()
+    elif cmd == 'back':
+        que.back()
